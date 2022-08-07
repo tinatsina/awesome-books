226b763dd4e4d5118233267420ae03e753bcb27f
tinayetsina <tinayetsina@outlook.com>
2022-08-08 04:25:06 +0800

    resolved minor errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
+import { DateTime } from '../modules/luxon.js';
 import getLocalStorage from '../modules/loadStorage.js';
 import bookList from '../modules/bookList.js';
 import switchPage from '../modules/pageChange.js';
 import bookOnClick from '../modules/bookListOnClick.js';
 
+console.log(DateTime.now());
 bookList(getLocalStorage());
 switchPage();
 bookOnClick();
